ecaef283fc102a249e26d07f2ee09480e58c1ba4
choijin <cjg030820@gmail.com>
2025-06-01 04:43:37 +0900

    과제 완성

--- task_birthday_flower/scripts/index.js
+++ task_birthday_flower/scripts/index.js
@@ -1,4 +1,4 @@
-const birthday_flower = [
+const birthdayFlower = [
     {
         month:1,
         flower:'수선화',
@@ -49,9 +49,69 @@ const birthday_flower = [
         content:'축하, 감사'
     }
 ]
+const mainBg = document.querySelector('main')
 const birthdayYears = document.querySelector('input[name=years]')
 const birthdayMonth = document.querySelector('input[name=month]')
 const birthdayDays = document.querySelector('input[name=days]')
 const flowerAnswer = document.querySelector('.flower_answer')
 const answerBtn = document.querySelector('#answer_btn')
+const flowerImg = document.querySelector('.flower_img')
 console.log(birthdayMonth, flowerAnswer, answerBtn)
+answerBtn.addEventListener('click', function(){
+    if(birthdayMonth.value == 1){
+        yourFlower(1);
+    }
+    if(birthdayMonth.value == 2){
+        yourFlower(2);
+    }
+    if(birthdayMonth.value == 3){
+        yourFlower(3);
+    }
+    if(birthdayMonth.value == 4){
+        yourFlower(4);
+    }
+    if(birthdayMonth.value == 5){
+        yourFlower(5);
+    }
+    if(birthdayMonth.value == 6){
+        yourFlower(6);
+    }
+    if(birthdayMonth.value == 7){
+        yourFlower(7);
+    }
+    if(birthdayMonth.value == 8){
+        yourFlower(8);
+    }
+    if(birthdayMonth.value == 9){
+        yourFlower(9);
+    }
+    if(birthdayMonth.value == 10){
+        yourFlower(10);
+    }
+    if(birthdayMonth.value == 11){
+        yourFlower(11);
+    }
+    if(birthdayMonth.value == 12){
+        yourFlower(12);
+    }
+})
+birthdayDays.addEventListener('click', dayMonth)
+birthdayMonth.addEventListener('click', dayMonth)
+function yourFlower(num){
+    flowerAnswer.innerHTML = `${birthdayFlower[num - 1].month}월 당신의 탄생화는 <em>${birthdayFlower[num - 1].flower}</em>입니다.<br>${birthdayFlower[num - 1].flower}의 꽃말은 <em>${birthdayFlower[num - 1].content}</em>입니다.`
+    return flowerImg.style.background = `url(./images/bg${num}.jpg) no-repeat center / cover`
+}
+function dayMonth(){
+    if(birthdayMonth.value > 12){
+        birthdayMonth.value = 12;
+    }
+    if(birthdayMonth.value < 1){
+        birthdayMonth.value = 1;
+    }
+    if(birthdayDays.value > 31){
+        birthdayDays.value = 31;
+    }
+    if(birthdayDays.value < 1){
+        birthdayDays.value = 1;
+    }
+}
