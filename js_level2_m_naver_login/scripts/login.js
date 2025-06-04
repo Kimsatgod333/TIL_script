fdeb6b22b914bb6db15446afe1b4f16988fda325
choijin <cjg030820@gmail.com>
2025-06-04 10:19:23 +0900

    로그인 에러 조건 1

--- js_level2_m_naver_login/scripts/login.js
+++ js_level2_m_naver_login/scripts/login.js
@@ -25,3 +25,22 @@ langOpen.style.display = 'none';//초기 숨김상태
 langSelect.addEventListener('click',()=>{
     langOpen.style.display == 'flex' ? langOpen.style.display = 'none' : langOpen.style.display = 'flex';
 })
+//네이버 로그인 에러 메세지 알고리즘
+//아이디를 입력한 상태에서 비밀번호를 입력하지 않은 상태에서 로그인 버튼 클릭시 '비밀번호를 입력해주세요' 문구 출력
+//아이디, 비밀번호를 입력후 로그인 클릭시 정보가 틀릴경우 아이디, 비밀번호 입력정보가 잘못되었습니다. 다시 입력해주세요. 문구 출력
+//로그인 실패가 3회이상 진행되었을때 가상영수증퀴즈와 아이디 비밀번호 자동입력 방지 문자를 잘못 입력하였습니다. 다시 확인해 주세요. 메세지 출력
+//아이디,비밀번호를 입력하지 않은상태(value='')에서 로그인 버튼을 누를경우 '아이디 또는 전화번호를 입력해주세요 라는 메세지 출력'
+const userId = document.querySelector('input[name=user_id]')
+const userPw = document.querySelector('input[name=user_pw]')
+const loginErrMsg = document.querySelector('.error_msg')
+const loginBtn = document.querySelector('#login_btn');
+console.log(loginErrMsg, userId, userPw, loginBtn)
+loginBtn.addEventListener('click',()=>{
+    /* if(userId.value == '' && userPw.value == ''){
+        loginErrMsg.textContent = '아이디 또는 전화번호를 입력해주세요'
+    } */
+    //삼항조건 활용 조건1
+    //조건식 ? 조건식 참결과 : 조건식 거짓결과
+    //조건결과대입변수 = 조건식 ? 조건식 참결과 : 조건식 거짓결과
+    loginErrMsg.textContent = userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해주세요' : '';
+})
